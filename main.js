62b3d3420c30dee583a8d735a30a987e48ca0b5a
Felipe4192 <ramos.silva.felipe1410@escola.pr.gov.br>
2026-08-03 13:47:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,25 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
-    if (tamanhoSenha>1){
+    if (tamanhoSenha > 1) {
 
         tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+campoSenha.value = letrasMaiusculas
